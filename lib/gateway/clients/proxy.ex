77411e4838c50e8ceb373e47ebd16ca8d20d23e3
Mario Macai <mario.macai@accenture.com>
2017-02-14 21:26:31 +0100

    fixed proxy responses

--- lib/gateway/clients/proxy.ex
+++ lib/gateway/clients/proxy.ex
@@ -4,7 +4,7 @@ defmodule Gateway.Clients.Proxy do
   """
   use HTTPoison.Base
 
-  def process_headers do
+  def process_headers(_headers) do
     [{"Content-Type", "application/json; charset=utf-8"}, {"Content-Encoding", "gzip"}]
   end
 end
