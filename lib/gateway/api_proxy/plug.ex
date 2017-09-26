6cd06f47d2d70d1278cdd5d0c75b83de8aea3396
Mario Macai <mario.macai@accenture.com>
2017-09-26 11:24:13 +0200

    removed unused import & fixed moduledoc typo

--- lib/gateway/api_proxy/plug.ex
+++ lib/gateway/api_proxy/plug.ex
@@ -1,10 +1,9 @@
 defmodule Gateway.ApiProxy.Plug do
   @moduledoc """
-  Module responsible for catching and forwarding all trafix to Proxy Router.
+  Module responsible for catching and forwarding all trafic to Proxy Router.
   Possible to do transformations to requests before they get to Proxy Router for
   further processing & forwarding.
   """
-  import Plug.Conn
 
   def init(opts), do: opts
   def call(conn, opts) do
