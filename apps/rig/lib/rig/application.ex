930d1cbade7b4165e8f64c9c0f069d210eb62c99
Mario Macai <mario.macai@accenture.com>
2018-03-01 15:43:40 +0100

    updated logger configuration and changelog

--- apps/rig/lib/rig/application.ex
+++ apps/rig/lib/rig/application.ex
@@ -2,14 +2,13 @@ defmodule Rig.Application do
   @moduledoc false
 
   use Application
-  use Rig.Config, []
+  use Rig.Config, [:log_level]
 
   def start(_type, _args) do
     alias Supervisor.Spec
 
     # Override application logging with environment variable
-    conf = config()
-    Logger.configure [{:level, conf.log_level}]
+    Logger.configure([{:level, config().log_level}])
 
     Rig.Discovery.start()
 
