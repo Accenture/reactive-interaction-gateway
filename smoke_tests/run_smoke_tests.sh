d44a2c27051aff38243d698f2b39027986ef66d3
Kevin Bader <kevin.bader@accenture.com>
2018-08-02 15:21:34 +0200

    Wait for Kafka readiness before running smoke tests

--- smoke_tests/run_smoke_tests.sh
+++ smoke_tests/run_smoke_tests.sh
@@ -15,6 +15,11 @@ docker build -f ../smoke_tests.dockerfile -t "${DOCKER_IMAGE}" ..
 section_header "Starting Kafka and backend-simulation services"
 $COMPOSE up -d kafka rest-api
 
+while [[ -z "$($COMPOSE exec kafka bash -c '/opt/kafka/bin/kafka-topics.sh --zookeeper $KAFKA_ZOOKEEPER_CONNECT --topic rig --describe')" ]]; do
+  echo "waiting for Kafka to start and create the topic.."
+  sleep 1
+done
+
 section_header "Running tests, plaintext Kafka connection"
 $COMPOSE run --rm \
   -e KAFKA_ENABLED=1 \
