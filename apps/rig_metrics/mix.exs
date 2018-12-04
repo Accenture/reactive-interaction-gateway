0ce1419ffa30a1319223a6cec1ed29bfb1bde9f7
Jakob Wagner <jaykob.wagner@gmail.com>
2018-12-04 14:29:50 +0100

    Update mix file

--- apps/rig_metrics/mix.exs
+++ apps/rig_metrics/mix.exs
@@ -2,33 +2,64 @@ defmodule RigMetrics.MixProject do
   use Mix.Project
 
   def project do
+    %{rig: rig_version, elixir: elixir_version} = versions()
+
     [
       app: :rig_metrics,
-      version: "0.1.0",
+      version: rig_version,
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.7",
+      elixir: elixir_version,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      aliases: aliases(),
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      applications: [
+        :prometheus_ex,
+        :prometheus_ecto,
+        :prometheus_phoenix,
+        :prometheus_plugs,
+        :prometheus_process_collector
+      ],
       extra_applications: [:logger],
       mod: {RigMetrics.Application, []}
     ]
   end
 
+  defp versions do
+    {map, []} = Code.eval_file("version", "../..")
+    map
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true},
+      {:prometheus_ex, "~> 3.0"},
+      {:prometheus_ecto, "~> 1.3"},
+      {:prometheus_phoenix, "~> 1.2"},
+      {:prometheus_plugs, "~> 1.1"},
+      {:prometheus_process_collector, "~> 1.4"}
     ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    []
   end
 end
