00d45417d39c7d2c98f8ffd216e454164f95daf2
Jakob Wagner <jaykob.wagner@gmail.com>
2018-12-04 14:33:55 +0100

    Remove autogenerated code

--- apps/rig_metrics/lib/rig_metrics.ex
+++ apps/rig_metrics/lib/rig_metrics.ex
@@ -2,17 +2,4 @@ defmodule RigMetrics do
   @moduledoc """
   Documentation for RigMetrics.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> RigMetrics.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- apps/rig_metrics/lib/rig_metrics/application.ex
+++ apps/rig_metrics/lib/rig_metrics/application.ex
@@ -1,19 +1,13 @@
 defmodule RigMetrics.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
+  @moduledoc """
+  This is the main entry point of the RigMetrics application.
+  """
   use Application
-
-  def start(_type, _args) do
-    # List all child processes to be supervised
-    children = [
-      # Starts a worker by calling: RigMetrics.Worker.start_link(arg)
-      # {RigMetrics.Worker, arg},
-    ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
+  def start(_type, _args) do
+    children = []
     opts = [strategy: :one_for_one, name: RigMetrics.Supervisor]
     Supervisor.start_link(children, opts)
   end
