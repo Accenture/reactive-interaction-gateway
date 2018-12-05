3b6e5b4cf86bd109ddcd1264d0f99d908e703662
Jakob Wagner <jaykob.wagner@gmail.com>
2018-12-06 00:10:25 +0100

    implemented metrics for blacklisted sessions

--- apps/rig_auth/mix.exs
+++ apps/rig_auth/mix.exs
@@ -41,6 +41,7 @@ defmodule RigAuth.Mixfile do
   defp deps do
     [
       {:rig, in_umbrella: true},
+      {:rig_metrics, in_umbrella: true},
       {:confex, "~> 3.3"},
       {:cowboy, "~> 1.0"},
       {:httpoison, "~> 1.3"},
