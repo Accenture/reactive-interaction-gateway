8e0208beba14d9462c0c49f130267ba16d008031
Jakob Wagner <jaykob.wagner@gmail.com>
2018-12-06 17:27:28 +0100

    fix code readability issues

--- apps/rig_api/test/rig_api/controllers/health_controller_test.exs
+++ apps/rig_api/test/rig_api/controllers/health_controller_test.exs
@@ -1,13 +1,13 @@
 defmodule RigApi.HealthControllerTest do
-    @moduledoc false
-    require Logger
-    use ExUnit.Case, async: true
-    use RigApi.ConnCase
-  
-    describe "GET /health" do
-      test "should return OK as text response" do
-        conn = build_conn() |> get("/health")
-        assert conn.resp_body =~ "OK"
-      end
+  @moduledoc false
+  require Logger
+  use ExUnit.Case, async: true
+  use RigApi.ConnCase
+
+  describe "GET /health" do
+    test "should return OK as text response" do
+      conn = build_conn() |> get("/health")
+      assert conn.resp_body =~ "OK"
     end
   end
+end
