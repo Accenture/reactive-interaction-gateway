ba1810f57fe63017eb447784ff68e865595c5514
Kevin Bader <kevin.bader@accenture.com>
2019-01-04 09:48:38 +0100

    Remove previous CloudEvents implementation attempts

--- apps/rig_cloud_events/lib/cloud_event_parser.ex
+++
@@ -1,8 +0,0 @@
-defmodule CloudEventParser do
-  @moduledoc false
-  @type t :: module
-  @callback parse(t) ::
-              {:ok, t}
-              | {:error, :invalid_json, json_decode_error :: any}
-              | {:error, :illegal_field, parse_error :: any}
-end
