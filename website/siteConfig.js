cff6a6381f6a1d175ae0d0f14e2c2d51d2a32128
Mario Macai <mario.macai@accenture.com>
2019-07-11 09:42:41 +0200

    added github link to docs header

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -2,14 +2,16 @@
 // site configuration options.
 
 /* List of projects/orgs using your project for the users page */
-const users = [{
-  caption: 'Accenture',
-  // You will need to prepend the image path with your baseUrl
-  // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-  image: 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Accenture.svg',
-  infoLink: 'https://www.accenture.com',
-  pinned: true,
-}, ];
+const users = [
+  {
+    caption: 'Accenture',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
+    image: 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Accenture.svg',
+    infoLink: 'https://www.accenture.com',
+    pinned: true
+  }
+];
 
 const baseUrl = '/reactive-interaction-gateway/';
 
@@ -21,7 +23,8 @@ const siteConfig = {
   projectName: 'reactive-interaction-gateway',
   organizationName: 'Accenture',
   repoUrl: `https://github.com/accenture/reactive-interaction-gateway`,
-  headerLinks: [{
+  headerLinks: [
+    {
       doc: 'intro',
       label: 'User Documentation'
     },
@@ -29,7 +32,11 @@ const siteConfig = {
       href: `${baseUrl}source_docs/index.html`,
       label: 'Source Documentation'
     },
-    { search: true },
+    {
+      href: 'https://github.com/Accenture/reactive-interaction-gateway',
+      label: 'GitHub'
+    },
+    { search: true }
     // { page: 'help', label: 'Help' },
     // { blog: true, label: 'Blog' },
   ],
@@ -41,18 +48,16 @@ const siteConfig = {
   favicon: 'img/favicon.png',
 
   colors: {
-    primaryColor: "#a89f63", // "#bca71c", //'#c68c3e', //'#293C88', //'#2E8555',
-    secondaryColor: "#808080", // "#bc1c49", //'#3972C1', //'#205C3B',
+    primaryColor: '#a89f63', // "#bca71c", //'#c68c3e', //'#293C88', //'#2E8555',
+    secondaryColor: '#808080' // "#bc1c49", //'#3972C1', //'#205C3B',
   },
 
   // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
-  copyright: 'Copyright © ' +
-    new Date().getFullYear() +
-    ' Accenture',
+  copyright: 'Copyright © ' + new Date().getFullYear() + ' Accenture',
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
-    theme: 'default',
+    theme: 'default'
   },
 
   // Add custom scripts here that would be placed in <script> tags
@@ -69,7 +74,7 @@ const siteConfig = {
   // template.
   algolia: {
     apiKey: '071fafffc3dde04e540162e0f3f68af4',
-    indexName: 'reactive-interaction-gateway',
+    indexName: 'reactive-interaction-gateway'
   }
 };
 
