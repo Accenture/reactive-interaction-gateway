fe00223d332355e75701675d7bceb03f5e607ac5
Kevin Bader <kevin.c.bader@gmail.com>
2019-09-10 10:38:14 +0200

    Use API v2 in docs and i9n tests

--- test/rig_tests/proxy/publish_to_event_stream/kafka_test.exs
+++ test/rig_tests/proxy/publish_to_event_stream/kafka_test.exs
@@ -62,7 +62,7 @@ defmodule RigTests.Proxy.PublishToEventStream.KafkaTest do
     endpoint_path = "/#{endpoint_id}"
 
     # We register the endpoint with the proxy:
-    rig_api_url = "http://localhost:#{@api_port}/v1/apis"
+    rig_api_url = "http://localhost:#{@api_port}/v2/apis"
     rig_proxy_url = "http://localhost:#{@proxy_port}"
 
     body =
@@ -121,7 +121,7 @@ defmodule RigTests.Proxy.PublishToEventStream.KafkaTest do
     endpoint_path = "/#{endpoint_id}"
 
     # We register the endpoint with the proxy:
-    rig_api_url = "http://localhost:#{@api_port}/v1/apis"
+    rig_api_url = "http://localhost:#{@api_port}/v2/apis"
     rig_proxy_url = "http://localhost:#{@proxy_port}"
 
     setup_req_body =
--- test/rig_tests/proxy/response_from/kinesis_test.exs
+++ test/rig_tests/proxy/response_from/kinesis_test.exs
@@ -58,7 +58,7 @@ defmodule RigTests.Proxy.ResponseFrom.KinesisTest do
     end)
 
     # We register the endpoint with the proxy:
-    rig_api_url = "http://localhost:#{@api_port}/v1/apis"
+    rig_api_url = "http://localhost:#{@api_port}/v2/apis"
     rig_proxy_url = "http://localhost:#{@proxy_port}"
 
     body =
