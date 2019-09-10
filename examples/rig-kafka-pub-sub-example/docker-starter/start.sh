3595530b90752efacdac6baf9293cb9d8b51654e
Max Glassie <C-Max.Glassie@charter.com>
2019-09-10 13:32:25 -0600

    updates docker starter so springboot app works properly

--- examples/rig-kafka-pub-sub-example/docker-starter/start.sh
+++ examples/rig-kafka-pub-sub-example/docker-starter/start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# build the Springboot docker image
+cd ../example
+docker build -t example -f Dockerfile .
+
+# build RIG docker image from project root
+cd ../../../
+docker build -t example -f Dockerfile .
+
+# back to docker starter
+cd examples/rig-kafka-pub-sub-example/docker-starter/
+
+# run docker compose
 set -e
 docker-compose down; docker-compose up --build -d
