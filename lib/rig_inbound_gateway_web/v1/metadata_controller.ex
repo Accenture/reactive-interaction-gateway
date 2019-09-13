81f17c02c71e8817af6218a3096ea6ed7768e076
Ariel Simulevski <ariel@simulevski.at>
2019-09-13 13:13:19 +0200

    Made gitignore usable for IntelliJ users

--- lib/rig_inbound_gateway_web/v1/metadata_controller.ex
+++ lib/rig_inbound_gateway_web/v1/metadata_controller.ex
@@ -1,6 +1,6 @@
 defmodule RigInboundGatewayWeb.V1.MetadataController do
   @moduledoc """
-  Handles inbound metadata sets and the indexing and distribution of metadata 
+  Handles inbound metadata sets and the indexing and distribution of metadata
   """
   use RigInboundGatewayWeb, :controller
 
@@ -21,7 +21,7 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
   Sets metadata KV pairs for an existing connection, replacing previous metadata sets.
 
   There may be indexed metadata pairs defined. If a metadata set does not contain these indexed keys, it cannot be accepted.
-  
+
   ## Example
 
   Metadata set that contains a user ID, locale and timezone:
@@ -34,7 +34,7 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
         }
       }
 
-  In this example, the field "userid" might be indexed. In this case, the connection token would get associated with the value of the "userid" field (it will be possible to automatically take the user id from the JWT token (so that it cannot be changed by any other way) and index that; this will be serverside configuration). 
+  In this example, the field "userid" might be indexed. In this case, the connection token would get associated with the value of the "userid" field (it will be possible to automatically take the user id from the JWT token (so that it cannot be changed by any other way) and index that; this will be serverside configuration).
   In addition to that, the value of the "userid", "locale" and "timezone" fields would get associated to the connection token.
 
   To sum it up: the connection token is indexed by default, while all other fields can be indexed by configuration.
@@ -42,17 +42,17 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
   So the association would look like so:
 
       userid -> connection token
-      
+
       conection token ->
         userid
         locale
         timezone
 
-  All indexed fields are then being propagated to all other instances so they know where to find the corresponding metadata.
+  All indexed fields are then being propagated to all other instances, so they know where to find the corresponding metadata.
 
   ## Example
 
-  A user connects to RIG 1 and sends the metadata fields from above to RIG 1. RIG 1 would propagate the connection token and the userid along with the RIG instance ID to all other RIG instances.
+  A user connects to RIG 1 and sends the metadata fields from above to RIG 1. RIG 1 would propagate the connection token and the userid along with the RIG instance ID to all other RIG instances (via gen_tcp, preferably).
   If someone were to access the metadata of a user from RIG 3, RIG 3 would know where to find the metadata, ask RIG 1 for it and return the metadata set.
 
   """
@@ -64,7 +64,7 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
       }
   ) do
     IO.puts "REACHED SET METADATA"
-    IO.puts(BodyReader.read_full_body(conn) |> elem(1))
+    IO.puts(inspect(BodyReader.read_full_body(conn)))
 
     send_resp(conn, :no_content, "")
   end
