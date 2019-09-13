2a63f290007f2302a7e146e8fe291f35acd12715
Ariel Simulevski <ariel@simulevski.at>
2019-09-13 10:15:43 +0200

    Added doc

--- lib/rig_inbound_gateway_web/v1/metadata_controller.ex
+++ lib/rig_inbound_gateway_web/v1/metadata_controller.ex
@@ -1,4 +1,7 @@
 defmodule RigInboundGatewayWeb.V1.MetadataController do
+  @moduledoc """
+  Handles inbound metadata sets and the indexing and distribution of metadata 
+  """
   use RigInboundGatewayWeb, :controller
 
   alias Result
@@ -14,6 +17,45 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
 
   require Logger
 
+  @doc """
+  Sets metadata KV pairs for an existing connection, replacing previous metadata sets.
+
+  There may be indexed metadata pairs defined. If a metadata set does not contain these indexed keys, it cannot be accepted.
+  
+  ## Example
+
+  Metadata set that contains a user ID, locale and timezone:
+
+      {
+        "metadata": {
+          "userid": "9ab1bff2-a8d8-455c-b48a-50145d7d8e30",
+          "locale": "de-AT",+
+          "timezone": "GMT+1"
+        }
+      }
+
+  In this example, the field "userid" might be indexed. In this case, the connection token would get associated with the value of the "userid" field.
+  In addition to that, the value of the "userid", "locale" and "timezone" fields would get associated to the connection token.
+
+  To sum it up: the connection token is indexed by default, while all other fields can be indexed by configuration.
+
+  So the association would look like so:
+
+      userid -> connection token
+      
+      conection token ->
+        userid
+        locale
+        timezone
+
+  All indexed fields are then being propagated to all other instances so they know where to find the corresponding metadata.
+
+  ## Example
+
+  A user connects to RIG 1 and sends the metadata fields from above to RIG 1. RIG 1 would propagate the connection token and the userid along with the RIG instance ID to all other RIG instances.
+  If someone were to access the metadata of a user from RIG 3, RIG 3 would know where to find the metadata, ask RIG 1 for it and return the metadata set.
+
+  """
   @spec set_metadata(conn :: Plug.Conn.t(), params :: map) :: Plug.Conn.t()
   def set_metadata(
       %{method: "PUT"} = conn,
