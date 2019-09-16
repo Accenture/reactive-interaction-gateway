29d4f8656aa8e2916ac885200860b9fa48b6d5a1
Ariel Simulevski <ariel@simulevski.at>
2019-09-16 17:08:54 +0200

    Added more options for configuration

--- config/config.exs
+++ config/config.exs
@@ -118,7 +118,11 @@ config :rig, Rig.Discovery,
   dns_name: {:system, "DNS_NAME", "localhost"}
 
 config :rig, RigInboundGatewayWeb.V1.MetadataController,
-  indexed_metadata: ["userid", "locale"]
+  jwt_fields: %{"sub" => "userid"},
+  indexed_metadata: ["userid"]
+  # TODO: If needed, check jwt values against metadata values; error and return 404 if they don't match; lo prio; current state: if fields don't match, jwt is prio and metadata value for a specific key gets overwritten
+  # has_compare: false
+  # compare_strict: %{"jwt/userid" => "meta/userid"}
 
 import_config "#{Mix.env()}.exs"
 
--- lib/rig_inbound_gateway_web/v1/metadata_controller.ex
+++ lib/rig_inbound_gateway_web/v1/metadata_controller.ex
@@ -46,29 +46,40 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
 
   ## Example
 
-  A user connects to RIG 1 and sends the metadata fields from above to RIG 1. RIG 1 would propagate the connection token and the userid along with the RIG instance ID to all other RIG instances (via gen_tcp, preferably).
+  A user connects to RIG 1 and sends the metadata fields from above to RIG 1. RIG 1 would propagate the connection token and the userid along with the RIG instance ID to all other RIG instances.
   If someone were to access the metadata of a user from RIG 3, RIG 3 would know where to find the metadata, ask RIG 1 for it and return the metadata set.
 
   """
   @spec set_metadata(conn :: Plug.Conn.t(), params :: map) :: Plug.Conn.t()
   def set_metadata(
-      %{method: "PUT"} = conn,
-      %{
-         "connection_id" => connection_id
-      }
-  ) do
+        %{method: "PUT"} = conn,
+        %{
+          "connection_id" => connection_id
+        }
+      ) do
     conn
     |> accept_only_req_for(["application/json"])
     |> decode_metadata()
     |> do_set_metadata(connection_id)
   end
 
-  def decode_metadata(conn) do
+  defp decode_metadata(conn) do
     with {"application", "json"} <- content_type(conn),
          {:ok, body, conn} <- BodyReader.read_full_body(conn),
          {:ok, json} <- Jason.decode(body),
          {:parse, %{"metadata" => metadata}} <- {:parse, json},
          {:idx, true} <- {:idx, contains_idx?(metadata)} do
+
+      conf = config()
+      jwt_fields = conf.jwt_fields
+      
+      jwt_fields 
+      |> Enum.each(fn x ->
+        key = x |> elem(1)
+        metadata = Map.replace!(metadata, key, "!")
+        # TODO: Fix
+        IO.inspect metadata
+      end)
 
       conn
       |> Plug.Conn.assign(:metadata, metadata)
@@ -102,30 +113,31 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
     end
   end
 
-  def do_set_metadata(%{halted: true} = conn, _connection_id), do: conn
-  
-  def do_set_metadata(%{assigns: %{metadata: metadata}} = conn, connection_id) do
-    Logger.info inspect(metadata)
-    
-    conf = config()
-    indexed_fields = Enum.map(conf.indexed_metadata, fn x -> 
-      {x, metadata[x]}
-    end)
+  defp do_set_metadata(%{halted: true} = conn, _connection_id), do: conn
 
-    Logger.info inspect(indexed_fields)
+  defp do_set_metadata(%{assigns: %{metadata: metadata}} = conn, connection_id) do
+    Logger.info(inspect(metadata))
+
+    conf = config()
+
+    indexed_fields =
+      Enum.map(conf.indexed_metadata, fn x ->
+        {x, metadata[x]}
+      end)
+
+    Logger.info(inspect(indexed_fields))
 
     send_resp(conn, :no_content, "")
   end
 
-  def contains_idx?(metadata) do
+  defp contains_idx?(metadata) do
     conf = config()
     metadata_keys = Enum.map(metadata, fn x -> x |> elem(0) end)
     Enum.all?(conf.indexed_metadata, fn x -> Enum.member?(metadata_keys, x) end)
   end
 
-  def fail!(conn, msg) do
+  defp fail!(conn, msg) do
     send_resp(conn, :bad_request, msg)
     Plug.Conn.halt(conn)
   end
-
 end
