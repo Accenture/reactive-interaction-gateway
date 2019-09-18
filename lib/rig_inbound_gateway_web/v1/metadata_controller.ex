f88590eb55ea78c5c55e6e5ad2dd14f599deec25
Ariel Simulevski <ariel@simulevski.at>
2019-09-18 12:57:14 +0200

    Add unit tests, refactor metadata extraction

--- lib/rig_inbound_gateway_web/v1/metadata_controller.ex
+++ lib/rig_inbound_gateway_web/v1/metadata_controller.ex
@@ -83,32 +83,44 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
   # ---
 
   defp extract_metadata(conn) do
-    {json_metadata_err, json_metadata} = extract_metadata_from_json(conn)
-    jwt_metadata = extract_metadata_from_jwt(conn)
+    with {:ok, body, _conn} <- BodyReader.read_full_body(conn) do
+      {json_metadata_err, json_metadata} = body |> extract_metadata_from_json()
+      {_, jwt_metadata} = Map.get(conn.assigns, :auth_tokens, []) |> extract_metadata_from_jwt()
 
-    if json_metadata_err === :error do
-      {:error, json_metadata}
-    else
-      # Merge values from metadata with the values from JWT, prioritizing JWT values
-      metadata = Map.merge(json_metadata, jwt_metadata)
-
-      # Check if metadata contains all the indexed fields; if not: send back a bad request
-      # This needs to be done here because now the values from the JWT got inserted
-      if has_required_indices?(metadata) do
-        {:ok, metadata}
+      if json_metadata_err === :error do
+        {:error, json_metadata}
       else
-        message = """
-        Metadata doesn't contain indexed fields.
-        """
-        {:error, message}
+        # Merge values from metadata with the values from JWT, prioritizing JWT values
+        metadata = Map.merge(json_metadata, jwt_metadata)
+
+        # Check if metadata contains all the indexed fields; if not: send back a bad request
+        # This needs to be done here because now the values from the JWT got inserted
+        if has_required_indices?(metadata) do
+          {:ok, metadata}
+        else
+          message = """
+          Metadata doesn't contain indexed fields.
+          """
+          {:error, message}
+        end
       end
+    else
+      error ->
+        message = """
+        Expected JSON encoded body.
+
+        Technical info:
+        #{inspect(error)}
+        """
+
+        {:error, message}
     end
   end
 
   # ---
 
-  defp extract_metadata_from_jwt(conn) do
-    with {:ok, claims} <- Map.get(conn.assigns, :auth_tokens, [])
+  def extract_metadata_from_jwt(auth_tokens) do
+    with {:ok, claims} <- auth_tokens
     |> Enum.map(fn
       {"bearer", token} -> JWT.parse_token(token)
       _ -> {:ok, %{}}
@@ -116,7 +128,7 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
     |> Result.list_to_result()
     |> Result.map_err(fn decode_errors -> {:error, decode_errors} end) do
       conf = config()
-      metadata_from_jwt = conf.jwt_fields
+      jwt_metadata_mapping = conf.jwt_fields
 
       # Merge the list of claims into a single map
       claims = Enum.reduce(claims, fn x, y ->
@@ -124,22 +136,20 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
       end)
 
       # Get values from JWT
-      for {key, jwt_key} <- metadata_from_jwt,
+      {:ok, (for {key, jwt_key} <- jwt_metadata_mapping,
         jwt_val = claims[jwt_key],
         into: %{},
-        do: {key, jwt_val}
+        do: {key, jwt_val})}
     else
       {:error, _} ->
-        %{}
+        {:error, %{}}
     end
   end
 
   # ---
 
-  defp extract_metadata_from_json(conn) do
-    with {"application", "json"} <- content_type(conn),
-         {:ok, body, _conn} <- BodyReader.read_full_body(conn),
-         {:ok, json} <- Jason.decode(body),
+  def extract_metadata_from_json(body) do
+    with {:ok, json} <- Jason.decode(body),
          {:parse, %{"metadata" => metadata}} <- {:parse, json} do
       {:ok, metadata}
     else
@@ -180,9 +190,14 @@ defmodule RigInboundGatewayWeb.V1.MetadataController do
 
   # ---
 
+  @doc """
+  Since RIG can propagate metadata through its network of RIG nodes, these datasets **need** to have all of the required indices (as specified in the configuration) for the mapping (index -> RIG instance where the data is actually located at) to work.
+
+  There are no *required* fields in RIG metadata, just required indices. Thus, this method is called `has_required_indices` and not `has_required_fields`.
+  """
   defp has_required_indices?(metadata) do
     conf = config()
-    metadata_keys = Enum.map(metadata, fn x -> x |> elem(0) end)
+    metadata_keys = Map.keys(metadata)
     Enum.all?(conf.indexed_metadata, fn x -> Enum.member?(metadata_keys, x) end)
   end
 
