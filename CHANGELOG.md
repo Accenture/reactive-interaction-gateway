f898c02f8b9096c7aed1cacb67ae044211f32241
Mario Macai <mario.macai@accenture.com>
2020-02-14 11:26:17 +0100

    277 - added types

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,7 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Added possibility to define Kafka/Kinesis topic and schema per reverse proxy endpoint. The current solution using environment variables is deprecated, but still used as a fallback -- will be removed in the version 3.0. [#229](https://github.com/Accenture/reactive-interaction-gateway/issues/229)
 - Added Kinesis + Localstack example [#229](https://github.com/Accenture/reactive-interaction-gateway/issues/229)
-- Added validation for reverse proxy configuration. Now it crashes RIG on start when configuration is not valid or return `400` when using REST API to modify APIs in reverse proxy. [#277](https://github.com/Accenture/reactive-interaction-gateway/issues/277)
+- Added validation for reverse proxy configuration. Now it crashes RIG on start when configuration is not valid or returns `400` when using REST API to update configuration. [#277](https://github.com/Accenture/reactive-interaction-gateway/issues/277)
 
 <!-- ### Changed -->
 
--- lib/rig_inbound_gateway/api_proxy/validations.ex
+++ lib/rig_inbound_gateway/api_proxy/validations.ex
@@ -8,14 +8,22 @@ defmodule RigInboundGateway.ApiProxy.Validations do
   """
   use Rig.Config, [:kinesis_request_stream, :kafka_request_topic, :kafka_request_avro]
 
+  alias RigInboundGateway.ApiProxy.Api
+
   require Logger
 
+  @type error_t :: [{:error, String.t() | atom, atom, String.t()}]
+  @type error_list_t :: [{String.t(), error_t()}]
+  @type error_map_t :: %{String.t() => [%{(String.t() | atom) => String.t()}]}
+
+  @spec validate_endpoint_target(Api.endpoint()) :: boolean
   def validate_endpoint_target(endpoint) do
     Vex.valid?(endpoint, %{"target" => [inclusion: ["kafka", "kinesis"]]})
   end
 
   # ---
 
+  @spec with_nested_presence(Api.t(), String.t() | atom, map) :: error_t()
   def with_nested_presence(true, key, map) do
     Vex.errors(map, %{key => [presence: true]})
   end
@@ -24,6 +32,7 @@ defmodule RigInboundGateway.ApiProxy.Validations do
 
   # ---
 
+  @spec validate_auth_type(Api.t()) :: error_t()
   def validate_auth_type(%{"auth" => %{"use_header" => true}} = api) do
     Vex.errors(api, %{"auth_type" => [inclusion: ["jwt"]]})
   end
@@ -36,6 +45,7 @@ defmodule RigInboundGateway.ApiProxy.Validations do
 
   # ---
 
+  @spec validate_auth(error_list_t(), Api.t()) :: error_list_t()
   def validate_auth(errors, %{"auth" => auth, "id" => id} = api) do
     use_header_error =
       Vex.errors(
@@ -69,6 +79,7 @@ defmodule RigInboundGateway.ApiProxy.Validations do
 
   # ---
 
+  @spec validate_secured_endpoint(Api.t(), Api.endpoint()) :: error_t()
   def validate_secured_endpoint(api, %{"secured" => true}) do
     Vex.errors(api, %{"auth_type" => [inclusion: ["jwt"]]})
   end
@@ -77,12 +88,14 @@ defmodule RigInboundGateway.ApiProxy.Validations do
 
   # ---
 
+  @spec with_any_error(error_list_t(), integer) :: error_list_t()
   def with_any_error(errors, min_errors \\ 1)
   def with_any_error(errors, min_errors) when length(errors) > min_errors, do: errors
   def with_any_error(_, _), do: []
 
   # ---
 
+  @spec validate_endpoints(error_list_t(), Api.t()) :: error_list_t()
   def validate_endpoints(
         errors,
         %{"id" => id, "version_data" => %{"default" => %{"endpoints" => endpoints}}} = api
@@ -133,6 +146,7 @@ defmodule RigInboundGateway.ApiProxy.Validations do
 
   # ---
 
+  @spec validate_all(Api.t()) :: error_list_t()
   def validate_all(api) do
     []
     |> validate_auth(api)
@@ -142,14 +156,12 @@ defmodule RigInboundGateway.ApiProxy.Validations do
 
   # ---
 
+  @spec validate!(Api.t()) :: Api.t()
   def validate!(api) do
     errors = validate_all(api)
 
     if errors != [] do
-      Logger.error(fn ->
-        "Wrong reverse proxy configuration: #{inspect(errors)}"
-      end)
-
+      log_error(errors)
       Process.exit(self(), :ReverseProxyConfigurationError)
     end
 
@@ -158,14 +170,12 @@ defmodule RigInboundGateway.ApiProxy.Validations do
 
   # ---
 
+  @spec validate(Api.t()) :: {:error, error_list_t()} | {:ok, Api.t()}
   def validate(api) do
     errors = validate_all(api)
 
     if errors != [] do
-      Logger.error(fn ->
-        "Wrong reverse proxy configuration: #{inspect(errors)}"
-      end)
-
+      log_error(errors)
       {:error, errors}
     else
       {:ok, api}
@@ -174,11 +184,20 @@ defmodule RigInboundGateway.ApiProxy.Validations do
 
   # ---
 
-  def to_map(errors) do
+  @spec to_map(error_list_t()) :: error_map_t()
+  def(to_map(errors)) do
     errors
     |> Enum.map(fn {key, value} ->
       {key, Enum.map(value, fn {_, key, _, reason} -> %{key => reason} end)}
     end)
     |> Enum.into(%{})
+  end
+
+  # ---
+
+  defp log_error(errors) do
+    Logger.error(fn ->
+      "Wrong reverse proxy configuration: #{inspect(errors)}"
+    end)
   end
 end
