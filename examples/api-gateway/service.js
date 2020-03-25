89831ba74dd42de4803675ab162dc01686640b5c
Knappek <andy.knapp.ak@gmail.com>
2020-03-25 22:41:43 +0100

    log tracing information in example/api-gateway

--- examples/api-gateway/service.js
+++ examples/api-gateway/service.js
@@ -2,6 +2,7 @@ const http = require("http");
 const port = 3000;
 const handler = (req, res) => {
   console.log(`Request URL: ${req.url}`);
+  console.log(JSON.stringify(req.headers));
   res.end("Hi, I'm a demo service!\n");
 }
 const server = http.createServer(handler);
