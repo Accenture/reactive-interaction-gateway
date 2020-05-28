2d440cd2006527cddeccc2cacfff56ce88d391f1
Kevin Bader <kevin.c.bader@gmail.com>
2020-05-28 18:16:00 +0200

    Fix NATS major version for i9n tests

--- integration_tests/nats_tests/run.sh
+++ integration_tests/nats_tests/run.sh
@@ -1,45 +1,46 @@
 #!/usr/bin/env sh
 
 cd "$(dirname "$0")"
-rig_dir=../..
+RIG_DIR=../..
 
-container_prefix=rig-i9n-nats-test
-nats_container="${container_prefix}-nats"
+NATS_VERSION=2
+CONTAINER_PREFIX=rig-i9n-nats-test
+NATS_CONTAINER="${CONTAINER_PREFIX}-nats"
 
 function cleanup() {
-  printf "\nCleaning up..\n"
-  echo "killing the http client.."
-  kill %1
-  echo "stopping RIG.."
-  ${rig_dir}/_build/dev/rel/rig/bin/rig stop
-  echo "stopping (and removing) the NATS server.."
-  docker stop $nats_container
-  printf "\nCleanup complete.\n"
+    printf "\nCleaning up..\n"
+    echo "killing the http client.."
+    kill %1
+    echo "stopping RIG.."
+    ${RIG_DIR}/_build/dev/rel/rig/bin/rig stop
+    echo "stopping (and removing) the NATS server.."
+    docker stop $NATS_CONTAINER
+    printf "\nCleanup complete.\n"
 }
 trap cleanup EXIT
 
 # Start NATS server:
-docker stop $nats_container 2>/dev/null
-docker run --rm -d --name $nats_container -p 4222:4222 nats
+docker stop $NATS_CONTAINER 2>/dev/null
+docker run --rm -d --name $NATS_CONTAINER -p 4222:4222 nats:$NATS_VERSION
 sleep 1 # should be up within 1 sec
 
 # Build and run RIG:
-(cd $rig_dir && mix distillery.release)
+(cd $RIG_DIR && mix distillery.release)
 export NATS_SERVERS=localhost:4222
 export NATS_SOURCE_TOPICS=rig-test
-${rig_dir}/_build/dev/rel/rig/bin/rig stop 2>/dev/null
-${rig_dir}/_build/dev/rel/rig/bin/rig start
+${RIG_DIR}/_build/dev/rel/rig/bin/rig stop 2>/dev/null
+${RIG_DIR}/_build/dev/rel/rig/bin/rig start
 
 # Wait for RIG to start its webserver:
 printf "waiting for RIG to start up"
 secs_left=30
 while ! http --check-status --print '' :4000/_rig/health 2>/dev/null; do
-  if [[ $secs_left > 0 ]]; then
-    printf "." ; sleep 1 ; secs_left=$((secs_left-1))
-  else
-    printf " - timed out."
-    exit 1
-  fi
+    if [[ $secs_left > 0 ]]; then
+        printf "." ; sleep 1 ; secs_left=$((secs_left-1))
+    else
+        printf " - timed out."
+        exit 1
+    fi
 done
 printf "\n"
 
@@ -50,12 +51,12 @@ http --stream :4000/_rig/v1/connection/sse\?subscriptions='[{"eventType":"test"}
 printf "waiting for the client to be connected and subscribed"
 secs_left=30
 while ! grep -q rig.subscriptions_set received; do
-  if [[ $secs_left > 0 ]]; then
-    printf "." ; sleep 1 ; secs_left=$((secs_left-1))
-  else
-    printf " - timed out."
-    exit 1
-  fi
+    if [[ $secs_left > 0 ]]; then
+        printf "." ; sleep 1 ; secs_left=$((secs_left-1))
+    else
+        printf " - timed out."
+        exit 1
+    fi
 done
 printf "\n"
 
@@ -66,13 +67,13 @@ mix deps.get && mix run publish_event_to_nats_topic.exs
 printf "waiting for events to show up in the client's output"
 secs_left=30
 while ! grep -q "the event was consumed from a NATS topic and published to the client" received; do
-  if [[ $secs_left > 0 ]]; then
-    printf "." ; sleep 1 ; secs_left=$((secs_left-1))
-  else
-    printf " - timed out."
-    printf "\nTEST FAILED!\n"
-    exit 1
-  fi
+    if [[ $secs_left > 0 ]]; then
+        printf "." ; sleep 1 ; secs_left=$((secs_left-1))
+    else
+        printf " - timed out."
+        printf "\nTEST FAILED!\n"
+        exit 1
+    fi
 done
 printf "\n"
 
