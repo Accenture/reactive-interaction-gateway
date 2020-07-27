9eb9421acea55bec80b9ccc534266e032207156b
Knappek <andy.knapp.ak@gmail.com>
2020-07-27 10:45:47 +0200

    fix ui tests because rig orders the cloudevent output alphabetically

--- examples/tests/cypress/integration/channels.spec.js
+++ examples/tests/cypress/integration/channels.spec.js
@@ -6,7 +6,7 @@ describe('Channels', () => {
       cy.subscribe('mike', 'my.public.event');
       // "foo":"bar" due to way how Cypress handles escaping of curly braces
       cy.sendEvent('my.public.event', '"foo":"bar"');
-      cy.assertEventLogHasMatch('(.*my.public.event)(.*"foo":"bar")');
+      cy.assertEventLogHasMatch('(.*"foo":"bar")(.*"eventType":"my.public.event")');
       cy.disconnect();
     });
 
@@ -16,11 +16,11 @@ describe('Channels', () => {
       cy.subscribe('mike', 'message');
       // "name":"mike","foo":"bar" due to way how Cypress handles escaping of curly braces
       cy.sendEvent('message', '"name":"mike","foo":"bar"');
-      cy.assertEventLogHasMatch('"name":"mike","foo":"bar"');
+      cy.assertEventLogHasMatch('.*"foo":"bar",.*"name":"mike"');
       // send constrained event for different user - John
       cy.sendEvent('message', '"name":"john","foo":"bar"');
       // Mike shouldn't receive any new event
-      cy.assertEventLogHasMatch('(.*message)(.*"name":"mike","foo":"bar")');
+      cy.assertEventLogHasMatch('(.*"foo":"bar",.*"name":"mike")(.*"eventType":"message")');
       cy.disconnect();
     });
   });
