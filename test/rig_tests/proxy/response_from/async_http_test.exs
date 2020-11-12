83918ff0b26585f64839f7f23cd1f10ac1bcbd5a
Mario Macai <mario.macai@accenture.com>
2020-11-12 19:11:32 +0100

    278 - fixed new unit tests

--- test/rig_tests/proxy/response_from/async_http_test.exs
+++ test/rig_tests/proxy/response_from/async_http_test.exs
@@ -198,7 +198,7 @@ defmodule RigTests.Proxy.ResponseFrom.AsyncHttpTest do
                 id: endpoint_id,
                 type: "http",
                 method: "GET",
-                path: endpoint_path,
+                path_regex: endpoint_path,
                 response_from: "http_async"
               }
             ]
@@ -268,7 +268,7 @@ defmodule RigTests.Proxy.ResponseFrom.AsyncHttpTest do
                 id: endpoint_id,
                 type: "http",
                 method: "GET",
-                path: endpoint_path,
+                path_regex: endpoint_path,
                 response_from: "http_async"
               }
             ]
